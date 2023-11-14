436d62806fe6764f23677ac5d773cd1f96975b19
jungyunmok <jungyunmok@gmail.com>
2023-11-14 17:08:57 +0900

    feat: 날짜 입력 메서드

--- src/main/java/christmas/domain/Exception.java
+++ src/main/java/christmas/domain/Exception.java
@@ -1,12 +1,11 @@
 package christmas.domain;
 
 public class Exception {
-    public int checkInt(String input , String errorMsg) {
+    public int checkInt(String input) {
         int number;
         try {
             number = Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            System.out.println(errorMsg);
             throw new IllegalArgumentException(e);
         }
         return number;
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,7 +1,22 @@
 package christmas.view;
 
-
 import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.Exception;
 
 public class InputView {
+    Exception exception = new Exception();
+    public int readDate() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String input = Console.readLine();
+        int date = 0;
+        while(date == 0) {
+            date = exception.checkInt(input);
+            if (date < 1 || date > 31) {
+                System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+                date = 0;
+            }
+        }
+        return date;
+    }
 }
