c8cc3fd19c478c32997f18cd84c764c739795c3d
jungyunmok <jungyunmok@gmail.com>
2023-11-14 20:19:57 +0900

    feat: 각 주문을 해시맵에 담기

--- src/main/java/christmas/domain/Exception.java
+++ src/main/java/christmas/domain/Exception.java
@@ -16,16 +16,30 @@ public class Exception {
     }
 
     public boolean checkOrder(String input) {
+        try {
+            String[] items = input.split(",");
+            for (String item : items) {
+                checkMenu(item);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(e);
+        }
+        return true;
+    }
+
+    public void checkMenu(String item) {
         Map<String, Integer> items = new HashMap<>();
-        String[] tempInputs = input.split(",");
-        for (String tempItems : tempInputs) {
-            String[] tempItem = tempItems.split("-");
+        try {
+            String[] tempItem = item.split("-");
             if (items.containsKey(tempItem[0])) {
-                return false;
+                throw new IllegalArgumentException();
             }
             items.put(tempItem[0], checkInt(tempItem[1]));
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(e);
         }
         order = new Order(items);
-        return true;
     }
 }
--- src/test/java/christmas/domain/ExceptionTest.java
+++ src/test/java/christmas/domain/ExceptionTest.java
@@ -28,6 +28,4 @@ class ExceptionTest {
          assertThatThrownBy(() -> exception.checkInt("test"))
             .isInstanceOf(IllegalArgumentException.class);
     }
-
-
 }
