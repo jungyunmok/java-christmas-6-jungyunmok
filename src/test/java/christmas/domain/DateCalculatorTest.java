bdc2af753b87e9e7b732f62d68494780814f84e9
jungyunmok <jungyunmok@gmail.com>
2023-11-14 18:48:17 +0900

    test: 요일 반환 테스트

--- src/test/java/christmas/domain/DateCalculatorTest.java
+++ src/test/java/christmas/domain/DateCalculatorTest.java
@@ -10,4 +10,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DateCalculatorTest {
+    private DateCalculator dateCalculator;
+
+    @BeforeEach
+    void setUp() {
+        dateCalculator = new DateCalculator();
+    }
+
+    @DisplayName("일자 입력시 해당하는 날짜 반환 - 금요일")
+    @ValueSource(ints = {1, 8, 15, 22, 29})
+    @ParameterizedTest
+    void 금요일_반환(int input) {
+        assertThat(dateCalculator.findDay(input)).isEqualTo("금");
+    }
+
+    @DisplayName("일자 입력시 해당하는 날짜 반환- 월요일")
+    @ValueSource(ints = {4, 11, 18, 25})
+    @ParameterizedTest
+    void 월요일_반환(int input) {
+        assertThat(dateCalculator.findDay(input)).isEqualTo("월");
+    }
 }
